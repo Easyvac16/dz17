3c00ada3a69b958c672adf8f94b528c3c603b325
Easyvac16 <gdima9051@gmail.com>
2023-05-29 20:18:26 +0300

    Little changes

--- dz17/cs1.cs
+++ dz17/cs1.cs
@@ -6,6 +6,29 @@ using System.Threading.Tasks;
 
 namespace dz17
 {
+    public class Employee
+    {
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string ContactPhone { get; set; }
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(string fullName, string position, string contactPhone, string email, decimal salary)
+        {
+            FullName = fullName;
+            Position = position;
+            ContactPhone = contactPhone;
+            Email = email;
+            Salary = salary;
+        }
+
+        public string GetInfo()
+        {
+            return $"ПІБ: {FullName}\nПосада: {Position}\nКонтактний телефон: {ContactPhone}\nEmail: {Email}\nЗаробітна плата: {Salary}";
+        }
+    }
+
     public class Firm
     {
         public string Name { get; set; }
@@ -47,29 +70,6 @@ namespace dz17
             }
 
             return info;
-        }
-    }
-
-    public class Employee
-    {
-        public string FullName { get; set; }
-        public string Position { get; set; }
-        public string ContactPhone { get; set; }
-        public string Email { get; set; }
-        public decimal Salary { get; set; }
-
-        public Employee(string fullName, string position, string contactPhone, string email, decimal salary)
-        {
-            FullName = fullName;
-            Position = position;
-            ContactPhone = contactPhone;
-            Email = email;
-            Salary = salary;
-        }
-
-        public string GetInfo()
-        {
-            return $"ПІБ: {FullName}\nПосада: {Position}\nКонтактний телефон: {ContactPhone}\nEmail: {Email}\nЗаробітна плата: {Salary}";
         }
     }
 
